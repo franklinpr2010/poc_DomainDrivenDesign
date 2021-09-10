7c11c2ddc0b448441e880cd7380a2cec9d4aecab
GFT\fnpa <franklinpr2010@gmail.com>
2021-09-09 21:34:25 -0300

    subindo projeto

--- src/main/java/br/com/alura/escola/MatricularAlunoPorLinhaDeComando.java
+++ src/main/java/br/com/alura/escola/MatricularAlunoPorLinhaDeComando.java
@@ -1,8 +1,12 @@
 package br.com.alura.escola;
 
-import br.com.alura.escola.aplicacao.aluno.matricular.MatricularAluno;
-import br.com.alura.escola.aplicacao.aluno.matricular.MatricularAlunoDto;
-import br.com.alura.escola.infra.aluno.RepositorioDeAlunosEmMemoria;
+import br.com.alura.escola.academico.aplicacao.aluno.matricular.MatricularAluno;
+import br.com.alura.escola.academico.aplicacao.aluno.matricular.MatricularAlunoDto;
+import br.com.alura.escola.academico.dominio.aluno.LogDeAlunoMatriculado;
+import br.com.alura.escola.academico.infra.aluno.RepositorioDeAlunosEmMemoria;
+import br.com.alura.escola.gamificacao.aplicacao.GeraSeloAlunoNovato;
+import br.com.alura.escola.gamificacao.infra.selo.RepositorioDeSelosEmMemoria;
+import br.com.alura.escola.shared.dominio.evento.PublicadorDeEventos;
 
 public class MatricularAlunoPorLinhaDeComando {
 	
@@ -11,11 +15,18 @@ public class MatricularAlunoPorLinhaDeComando {
 		String cpf = "123.456.789-00";
 		String email = "fulano@email.com";
 		
-		MatricularAluno matricular = new MatricularAluno(new RepositorioDeAlunosEmMemoria());
-		matricular.executa(
-				new MatricularAlunoDto(nome, 
-						cpf, 
-						email));
+		MatricularAlunoDto dto = new MatricularAlunoDto(
+				nome, cpf, email);
+		
+		PublicadorDeEventos publicador = new PublicadorDeEventos();
+		publicador.adicionar(new LogDeAlunoMatriculado());
+		publicador.adicionar(new GeraSeloAlunoNovato(
+				new RepositorioDeSelosEmMemoria()));
+		
+		MatricularAluno matricular = new MatricularAluno(
+				new RepositorioDeAlunosEmMemoria(), publicador);
+		
+		matricular.executa(dto);
 	}
 
 }
